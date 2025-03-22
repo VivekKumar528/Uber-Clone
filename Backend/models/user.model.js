c2c33dffaa0b0036bfc3fa9fd32ae9afe041d01a
Vivek Kumar <vicky2820x@gmail.com>
2025-03-22 14:35:42 +0530

    user registration done

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const userScema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   fullname: {
     firstname: {
       type: String,
@@ -31,18 +31,18 @@ const userScema = new mongoose.Schema({
   },
 });
 
-userScema.methods.generateAuthToken = function () {
-  const token = jwt.sign({ _id: this, _id }, process.env.JWT_SECRET);
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET);
   return token;
 };
 
-userScema.methods.comparePassword = async function (password) {
+userSchema.methods.comparePassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
-  
-userScema.statics.hashPassword = async function (password) {
+
+userSchema.statics.hashPassword = async function (password) {
   return await bcrypt.hash(password, 10);
 };
-const userModel = mongoose.model("user", userScema);
+const userModel = mongoose.model("user", userSchema);
 
 module.exports = userModel;
